30a29ce40e276f1c781ea9dfc01ec8d893ccede1
Vinod Halaharvi <vinod.halaharvi@gmail.com>
2016-09-11 14:00:28 -0400

    init ..

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by vhalaharvi on 9/11/16.
+ */
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        if (args.length > 0)
+        {
+            System.out.format("Hello, %s!\n", args[0]);
+        }
+    }
+}
+
